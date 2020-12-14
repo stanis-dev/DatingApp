ee0e6b774f43fb739f869f562de2377f2af804c7
Stan <ssamisko@kiteris.com>
2020-12-14 01:08:52 +0100

    Client walking skeleton and SSL installed

---
+++ client/src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <h1>Dating App</h1>
+    <ul v-if="users.length > 0">
+      <li v-for="user in users" :key="user.id">{{ user.userName }}</li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import axios from 'axios'
+
+export default defineComponent({
+  name: 'HelloWorld',
+  data() {
+    return {
+      users: []
+    }
+  },
+  async created() {
+    await this.getUsers()
+  },
+  methods: {
+    async getUsers() {
+      const users = await axios.get('https://localhost:5001/api/users')
+      this.users = users.data
+    }
+  }
+})
+</script>
+
+<style scoped lang="scss"></style>
---
+++ client/vue.config.js
@@ -0,0 +1,11 @@
+const fs = require('fs')
+
+module.exports = {
+  devServer: {
+    https: {
+      key: fs.readFileSync('./ssl/server.key'),
+      cert: fs.readFileSync('./ssl/server.crt'),
+      https: true
+    }
+  }
+}
